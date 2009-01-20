313d9c02b954100a54d355301def4ac636248f1d
schluchz <schluchz@7811cd5a-88b9-11dd-ba2e-09f2ff3e973f>
2009-01-20 00:01:51 +0000

    timer added

--- timer/timer.h
+++ timer/timer.h
@@ -1,4 +1,4 @@
-/*  Copyright (c) 2006-2007, Philip Busch <broesel@studcs.uni-sb.de>
+/*  Copyright (c) 2006-2008, Philip Busch <philip@0xe3.com>
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -23,28 +23,20 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef DEFINES_H_
-#define DEFINES_H_
+#ifndef TIMER_H_
+#define TIMER_H_
 
-/* some useful defines */
+typedef struct timer_ {
+	clock_t user;
+	struct timeval real;
+} timer;
 
-#if defined(C99)  /* there is no macro C99 */
-#define DEBUG(...) \
-	{ printf("%s:%d:%s(): ", __FILE__, __LINE__, __func__); \
-	  printf(__VA_ARGS__); \
-	  puts(""); }
-#else
-#define DEBUG(msg) \
-	{ printf("%s:%d: ", __FILE__, __LINE__); \
-	  printf msg; \
-	  puts(""); }
-#endif
+timer *timer_init();
+void   timer_destroy(timer *t);
+void   timer_start(timer *t);
+void   timer_stop(timer *t);
+double timer_get_user(timer *t);
+double timer_get_real(timer *t);
+void   timer_print(timer *t);
 
-#define SWAP(a, b)  {a ^= b; b ^= a; a ^= b;}
-
-#define MAX(a, b) ((a) < (b) ? (b) : (a))
-#define MIN(a, b) ((a) > (b) ? (b) : (a))
-
-#define COMPUTE(e) printf(#e " = %d", e)
-
-#endif /* ! DEFINES_H_ */
+#endif  /* ! TIMER_H_ */
